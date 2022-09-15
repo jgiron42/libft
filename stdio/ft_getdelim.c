e47e7ff88411a24feb62cd57f3d3a8cb9bb36cb7
jgiron <jgiron@student.42.fr>
2022-09-15 19:14:40 +0200

    fix getline and fread and feof

--- stdio/ft_getdelim.c
+++ stdio/ft_getdelim.c
@@ -16,20 +16,24 @@ ssize_t  ft_getdelim(char **restrict lineptr, size_t *restrict n, int delim, ft_
 			return -1;
 		*n = to_read;
 	}
+//	printf("<>");
 	while (1) {
 		ret = ft_get_delim_buffered(*lineptr + read, to_read, stream, delim);
-		if (ret == -1)
+//		printf("=> [%s|%s|%zd|%zu]", *lineptr + read, *lineptr + read - 3, ret, read);
+		if (ret == -1 || (!ret && !read))
 			return -1;
 		read += ret;
-		if (!ret || *(*lineptr + read) == delim)
+		if (!ret || ft_feof(stream) || (read > 0 && *(*lineptr + read - 1) == delim))
 			break;
 		tmp = malloc(read + FT_STDIO_GETDELIM_BUFFER_SIZE);
 		if (!tmp)
 			return -1;
+		*n = read + FT_STDIO_GETDELIM_BUFFER_SIZE;
+		to_read = FT_STDIO_GETDELIM_BUFFER_SIZE;
 		memcpy(tmp, *lineptr, read + 1);
 		free(*lineptr);
 		*lineptr = tmp;
 	};
-	*n = read;
+//	printf("{%s}", *lineptr);
 	return read;
 }
