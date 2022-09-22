b886e80428fdefc0dad993b84d244b9672d3ca9d
joachim <jgiron@student.42.fr>
2022-09-22 10:24:02 +0200

    .

--- stdio/ft_setvbuf.c
+++ stdio/ft_setvbuf.c
@@ -2,8 +2,9 @@
 
 int		ft_setvbuf(ft_FILE *restrict stream, char *restrict buf, int type, size_t size)
 {
+	char *old_buf = NULL;
 	if (stream->wbuf && stream->free_wbuf)
-		free(stream->wbuf);
+		old_buf = stream->wbuf;
 	if (type != _IONBF)
 	{
 		if (buf)
@@ -19,10 +20,21 @@ int		ft_setvbuf(ft_FILE *restrict stream, char *restrict buf, int type, size_t s
 			stream->wbuf = buf;
 			stream->free_wbuf = true;
 		}
-		stream->wbuflen = size;
+		if (old_buf) {
+			if (stream->wbuflen <= size)
+				ft_memcpy(stream->wbuf, old_buf, stream->wbuflen);
+			else {
+				ft_memcpy(stream->wbuf, old_buf, size);
+				stream->wbuflen = size;
+			}
+		}
+		else
+			stream->wbuflen = 0;
+		stream->wbufcap = size;
 	}
 	else
 		stream->wbuf = NULL;
+	free(old_buf);
 	stream->buf_flag = type;
 	return 0;
 }
