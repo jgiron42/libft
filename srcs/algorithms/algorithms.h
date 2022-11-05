f8e9d2800fc3bd06c47ee18c7c9a577a06d0678c
joachim <jgiron@student.42.fr>
2022-11-05 23:21:28 +0100

    add lexicographical_compare

--- srcs/algorithms/algorithms.h
+++ srcs/algorithms/algorithms.h
@@ -2,4 +2,6 @@
 #define GREP_ALGORITHMS_H
 #include "../containers/containers.h"
 status	heap_sort(container *c);
+bool		ft_lexicographical_compare(iterator lbegin, iterator lend, iterator rbegin, iterator rend);
+int			ft_lexicographical_compare_three_way(iterator lbegin, iterator lend, iterator rbegin, iterator rend);
 #endif //GREP_ALGORITHMS_H
